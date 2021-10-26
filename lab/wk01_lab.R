929af6980cf90d9c414334f85cafc828d754668b
Chaithra K C <chaithra.kc22@gmail.com>
2021-10-26 00:37:45 -0400

    lab changes

---
+++ lab/wk01_lab.R
@@ -0,0 +1,23 @@
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 # adding the scalar values
+
+numbers <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) # creating vector
+
+sum(numbers) # sum function
+
+# vector math in which a calculation runs on every element of a vector
+updated_numbers <- numbers + 10
+
+sum(c(1:10)) # range of numbers
+
+# descriptive statistics
+mean(numbers)
+median(numbers)
+max(numbers)
+min(numbers)
+length(numbers)
+
+# filtering the data
+numbers>5 # returns vector of true/false
+bigNum <- numbers[numbers>5] # return the values having true
+
+help(mean)
---
+++ lab/wk02_lab.R
@@ -0,0 +1,1 @@
+myIris <- iris
